a80c38829a7e6220da0c367762c0d09be62d40ce
vanadium5000 <vanadium5000@gmail.com>
2025-06-01 12:15:31 +0100

    Fixed brightness buttons for mac & added device option to script

--- home-manager/scripts/brightness.nix
+++ home-manager/scripts/brightness.nix
@@ -10,30 +10,35 @@
   increments = "5";
   smallIncrements = "1";
 
-  # -d intel_backlight is a temporary solution, nvidia_0 is alternative and doesn't work
+# $2 is an optional argument which can include the device to change
   brightness-change = pkgs.writeShellScriptBin "brightness-change" ''
+  if [[ -n $3 ]]; then
+    [[ $1 == "up" ]] && ${pkgs.brightnessctl}/bin/brightnessctl -d "$3" set ''${2-${increments}}%+
+    [[ $1 == "down" ]] && ${pkgs.brightnessctl}/bin/brightnessctl -d "$3" set ''${2-${increments}}%-
+  else
     [[ $1 == "up" ]] && ${pkgs.brightnessctl}/bin/brightnessctl set ''${2-${increments}}%+
     [[ $1 == "down" ]] && ${pkgs.brightnessctl}/bin/brightnessctl set ''${2-${increments}}%-
-  '';
+  fi
+'';
 
   brightness-set = pkgs.writeShellScriptBin "brightness-set" ''
     ${pkgs.brightnessctl}/bin/brightnessctl set ''${1-100}%
   '';
 
   brightness-up = pkgs.writeShellScriptBin "brightness-up" ''
-    brightness-change up ${increments}
+    brightness-change up ${increments} $1
   '';
 
   brightness-down = pkgs.writeShellScriptBin "brightness-down" ''
-    brightness-change down ${increments}
+    brightness-change down ${increments} $1
   '';
 
   brightness-up-small = pkgs.writeShellScriptBin "brightness-up-small" ''
-    brightness-change up ${smallIncrements}
+    brightness-change up ${smallIncrements} $1
   '';
 
   brightness-down-small = pkgs.writeShellScriptBin "brightness-down-small" ''
-    brightness-change down ${smallIncrements}
+    brightness-change down ${smallIncrements} $1
   '';
 in {
   home.packages = [

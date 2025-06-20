140ade316541654dd330708afb2059cb39615525
vanadium5000 <vanadium5000@gmail.com>
2025-06-21 00:00:38 +0100

    grayscale

--- home-manager/desktop/hypr/hyprshade/default.nix
+++ home-manager/desktop/hypr/hyprshade/default.nix
@@ -6,17 +6,18 @@
       default = true
 
       [[shades]]
-      name = "a_greyscale"
-      start_time = 22:15:00
-      end_time = 06:30:00
-
-      [[shades]]
       name = "blue-light-filter"
       start_time = 19:00:00
-      end_time = 06:00:00
+      end_time = 22:15:00
+
+      [[shades]]
+      name = "grayscale"
+      start_time = 22:15:00
+      end_time = 06:30:00
     '';
   };
-  home.file.".config/hypr/shaders/a_greyscale.glsl".source = ./greyscale.glsl;
+  home.file.".config/hypr/shaders/grayscale.glsl".source = ./grayscale.glsl;
+  home.file.".config/hypr/shaders/blue-light-filter.glsl.mustache".source = ./blue-light-filter.glsl.mustache;
 
   wayland.windowManager.hyprland.settings = {
     exec = [

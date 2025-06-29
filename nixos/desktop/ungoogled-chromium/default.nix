aa20a6a25c712de16bbd5fc5b4ecba1c48adea21
vanadium5000 <vanadium5000@gmail.com>
2025-06-29 21:55:54 +0100

    ungoogled-chromium

--- nixos/desktop/ungoogled-chromium/default.nix
+++ nixos/desktop/ungoogled-chromium/default.nix
@@ -1,30 +1,29 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs.unstable; [
     ungoogled-chromium
   ];
 
-  programs = {
-    ungoogled-chromium = {
-      enable = true;
-      extensions = [
-        "kbfnbcaeplbcioakkpcpgfkobkghlhen" # Grammarly
-        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
-        "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock for YouTube
-        "gebbhagfogifgggkldgodflihgfeippi" # Return YouTube Dislike
+  programs.chromium = {
+    enable = true;
+
+    extensions = [
+      "kbfnbcaeplbcioakkpcpgfkobkghlhen" # Grammarly
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+      "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock for YouTube
+      "gebbhagfogifgggkldgodflihgfeippi" # Return YouTube Dislike
+    ];
+    extraOpts = {
+      "AutofillAddressEnabled" = false;
+      "AutofillCreditCardEnabled" = false;
+      "BuiltInDnsClientEnabled" = false;
+      "DeviceMetricsReportingEnabled" = false;
+      "ReportDeviceCrashReportInfo" = false;
+      "PasswordManagerEnabled" = false;
+      "SpellcheckEnabled" = true;
+      "SpellcheckLanguage" = [
+        "en-US"
       ];
-      extraOpts = {
-        "AutofillAddressEnabled" = false;
-        "AutofillCreditCardEnabled" = false;
-        "BuiltInDnsClientEnabled" = false;
-        "DeviceMetricsReportingEnabled" = false;
-        "ReportDeviceCrashReportInfo" = false;
-        "PasswordManagerEnabled" = false;
-        "SpellcheckEnabled" = true;
-        "SpellcheckLanguage" = [
-          "en-US"
-        ];
-        "VoiceInteractionHotwordEnabled" = false;
-      };
+      "VoiceInteractionHotwordEnabled" = false;
     };
   };
 }

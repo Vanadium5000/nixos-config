e45be7a8c0c587e056e30f1cfea4b64764f563fc
vanadium5000 <vanadium5000@gmail.com>
2025-07-05 19:48:09 +0100

    Doas

--- nixos/terminal/default.nix
+++ nixos/terminal/default.nix
@@ -41,39 +41,38 @@
   # Enable Polkit
   security.polkit.enable = true;
 
-  # # Doas is leaner and more secure
-  # security.doas = {
-  #   enable = true;
-
-  #   extraRules = [
-  #     {
-  #       # The usernames / UIDs this rule should apply for
-  #       users = [ config.var.username ];
-
-  #       # Optional, retains environment variables while running commands
-  #       # e.g. retains your NIX_PATH when applying your config
-  #       #keepEnv = true; # Problematic
-  #       persist = true; # Optional, don't ask for the password for some time, after a successfully authentication
-  #     }
-  #   ];
-  # };
-
-  # # Disable & replace Sudo
-  # security.sudo.enable = false;
-  # environment.shellAliases = {
-  #   sudo = "doas";
-  # };
-
-  # I would use doas but nixos-rebuild is a bitch
-  security.sudo = {
+  # Doas is leaner and more secure
+  security.doas = {
     enable = true;
-    extraConfig = ''
-      Defaults lecture = never
-      Defaults pwfeedback
-      Defaults insults
-      Defaults env_keep += "EDITOR PATH DISPLAY SUDO_ASKPASS"
-    '';
+
+    extraRules = [
+      {
+        # The usernames / UIDs this rule should apply for
+        users = [ config.var.username ];
+
+        # Optional, retains environment variables while running commands
+        # e.g. retains your NIX_PATH when applying your config
+        #keepEnv = true; # Problematic
+        persist = true; # Optional, don't ask for the password for some time, after a successfully authentication
+      }
+    ];
   };
+
+  # Disable & replace Sudo
+  security.sudo.enable = false;
+  environment.shellAliases = {
+    sudo = "doas";
+  };
+
+  # security.sudo = {
+  #   enable = true;
+  #   extraConfig = ''
+  #     Defaults lecture = never
+  #     Defaults pwfeedback
+  #     Defaults insults
+  #     Defaults env_keep += "EDITOR PATH DISPLAY SUDO_ASKPASS"
+  #   '';
+  # };
 
   # Fedora enables these options by default. See the 10-oomd-* files here:
   # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255

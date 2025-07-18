e6cc29ec22ec63ff67ee7bf29f657c6c23ede528
vanadium5000 <vanadium5000@gmail.com>
2025-07-18 18:40:25 +0100

    Remove custom waybar-lyric

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,4 @@
 pkgs: {
   nixos-wallpapers = pkgs.callPackage ./nixos-wallpapers.nix { };
   nixy-wallpapers = pkgs.callPackage ./nixy-wallpapers.nix { };
-
-  # TODO: Remove this due to it being in nixpkgs one I update
-  waybar-lyric = pkgs.callPackage ./waybar-lyric.nix { };
 }

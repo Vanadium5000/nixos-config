34ada165cac4c64a2dce30b2ccd1ec8c2484d8eb
vanadium5000 <vanadium5000@gmail.com>
2025-09-06 18:28:22 +0100

    Reduce vps ram usage 2

--- hosts/ionosvps/default.nix
+++ hosts/ionosvps/default.nix
@@ -25,9 +25,11 @@
   ];
 
   # Reduce ram usage
-  max-jobs = "1"; # Use only 1 core for building - default is 1, int or "auto"
-  cores = 1; # Use only 1 per build job
-  # Reduce binary cache ram usage
-  http-connections = 8; # Max number of parallel TCP connections - default is 25
-  max-substitution-jobs = 4; # Max number of substitution jobs in parallel - default is 16
+  nix.settings = {
+    max-jobs = "1"; # Use only 1 core for building - default is 1, int or "auto"
+    cores = 1; # Use only 1 per build job
+    # Reduce binary cache ram usage
+    http-connections = 8; # Max number of parallel TCP connections - default is 25
+    max-substitution-jobs = 4; # Max number of substitution jobs in parallel - default is 16
+  };
 }

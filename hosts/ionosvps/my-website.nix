3220d3011249403559e970543a473bf8d308453f
vanadium5000 <vanadium5000@gmail.com>
2025-10-27 23:30:13 +0000

    fix vps

--- hosts/ionosvps/my-website.nix
+++ hosts/ionosvps/my-website.nix
@@ -35,7 +35,7 @@
       # New auth proxy (preserves /auth/api/ path)
       locations."/auth/api/" = {
         proxyPass = "http://127.0.0.1:3000"; # The lack of a trailing / on proxy_pass in the auth block ensures the full path (/auth/api/*) is forwarded to your backend server at port 3000
-        proxyWebsockets = "on"; # If needed for WS
+        proxyWebsockets = true; # If needed for WS
         # Add other proxy settings like proxy_set_header Host $host; etc.
       };
 
